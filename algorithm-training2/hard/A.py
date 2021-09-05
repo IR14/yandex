f9bdd1094c283fcfcac6c8a1400493d1bc47eeaf
IR14 <iamikhaylov@edu.hse.ru>
2021-09-05 20:53:17 +0300

    Create A.py

---
+++ algorithm-training2/hard/A.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+if a == 0 and b == 0:
+	print("INF")
+
+elif a != 0 and b * c != a * d and b % a == 0:
+	print(-b // a)
+
+else:
+	print("NO")
