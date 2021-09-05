c517a03eebe829dc80229363cb8d62458435d1a9
IR14 <iamikhaylov@edu.hse.ru>
2021-09-05 18:32:32 +0300

    Create A.py

---
+++ algorithm-training2/A.py
@@ -0,0 +1,27 @@
+endCode = int(input())
+interactor = int(input())
+checker = int(input())
+
+if interactor == 0:
+  if endCode == 0:
+    print(checker)
+  else:
+    print(3)
+
+elif interactor == 1:
+  print(checker)
+
+elif interactor == 4:
+  if endCode == 0:
+    print(4)
+  else:
+    print(3)
+
+elif interactor == 6:
+  print(0)
+
+elif interactor == 7:
+  print(1)
+
+else:
+  print(interactor)
